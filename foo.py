deaa7d16fa6c97a98f6dca5ed5d1cba041e6cf2b
matthew <wczmatthew@gmail.com>
2019-01-20 20:23:22 +0800

    matthew: Update foo.py to use return N instead of return (N)

--- foo.py
+++ foo.py
@@ -5,14 +5,14 @@ import sys
 def main(argc, argv):
     if argc != 2:
         sys.stderr.write("Usage: %s <num>\n" % argv[0])
-        return (-1)
+        return -1
 
     i = 0
     while i < int(argv[1]):
         print "%02d: Hello World." % i
         i += 1
 
-    return (0)
+    return 0
 
 if __name__ == '__main__':
     argv = sys.argv
